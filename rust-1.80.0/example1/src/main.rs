2c285088fd84785e2b96626cc4848ce880fe65ca
Hentioe <meow.i5.br@gmail.com>
2024-07-30 06:11:07 +0800

    Update codes

--- rust-1.80.0/example1/src/main.rs
+++ rust-1.80.0/example1/src/main.rs
@@ -1,8 +1,9 @@
 use lazy_static::lazy_static;
-use std::{cell::LazyCell, collections::HashMap};
+use std::{collections::HashMap, sync::LazyLock};
 
 type MyMap = HashMap<u32, &'static str>;
 
+// 用 lazy_static 定义一个全局的 HashMap
 #[cfg(feature = "lazy_static")]
 lazy_static! {
     static ref LAZY_STATIC_MAP: MyMap = {
@@ -14,7 +15,8 @@ lazy_static! {
     };
 }
 
-const LAZY_CELL_MAP: LazyCell<MyMap> = LazyCell::new(|| {
+// 用 LazyLock 定义一个全局的 HashMap
+static LAZY_CELL_MAP: LazyLock<MyMap> = LazyLock::new(|| {
     let mut m = HashMap::new();
     m.insert(0, "foo");
     m.insert(1, "bar");
@@ -24,5 +26,5 @@ const LAZY_CELL_MAP: LazyCell<MyMap> = LazyCell::new(|| {
 
 fn main() {
     println!("lazy_static example: {:?}", *LAZY_STATIC_MAP); // => lazy_static example: {2: "baz", 0: "foo", 1: "bar"}
-    println!("lazy_cell example: {:?}", *LAZY_CELL_MAP); // => lazy_cell example: {2: "baz", 0: "foo", 1: "bar"}
+    println!("lazy_lock example: {:?}", *LAZY_CELL_MAP); // => lazy_cell example: {2: "baz", 0: "foo", 1: "bar"}
 }
