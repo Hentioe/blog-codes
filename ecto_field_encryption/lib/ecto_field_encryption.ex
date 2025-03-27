658f4385dbcc2c953fcc510cfaaa98cd26277aa9
Hentioe <meow.i5.br@gmail.com>
2025-03-27 23:49:41 +0800

    Add ecto_field_encryption

---
+++ ecto_field_encryption/lib/ecto_field_encryption.ex
@@ -0,0 +1,18 @@
+defmodule EctoFieldEncryption do
+  @moduledoc """
+  Documentation for `EctoFieldEncryption`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoFieldEncryption.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ecto_field_encryption/test/ecto_field_encryption_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoFieldEncryptionTest do
+  use ExUnit.Case
+  doctest EctoFieldEncryption
+
+  test "greets the world" do
+    assert EctoFieldEncryption.hello() == :world
+  end
+end
